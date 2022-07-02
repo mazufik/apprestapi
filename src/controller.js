57e089cee73ec8740dd186bacd4e54e9b675b8b5
Taufik Rahman <taufikprincecode10@gmail.com>
2022-07-02 16:33:49 +0900

    membuat route menampilkan data

--- src/controller.js
+++ src/controller.js
@@ -6,3 +6,14 @@ var connection = require('./koneksi');
 exports.index = function(req, res){
     response.ok('Aplikasi REST API berjalan', res)
 };
+
+//menampilkan semua data mahasiswa
+exports.tampilmahasiswa = function(req,res){
+    connection.query('select * from mahasiswa', function(error, rows, fileds){
+        if(error){
+            connection.log(error);
+        }else {
+            response.ok(rows, res)
+        }
+    });
+};
