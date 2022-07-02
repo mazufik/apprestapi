d9aab88b852f63432e374fdcdc82597a6536890d
Taufik Rahman <taufikprincecode10@gmail.com>
2022-07-02 16:40:12 +0900

    menambahkan route menampilkan data berdasarkan id

--- src/controller.js
+++ src/controller.js
@@ -9,7 +9,19 @@ exports.index = function(req, res){
 
 //menampilkan semua data mahasiswa
 exports.tampilmahasiswa = function(req,res){
-    connection.query('select * from mahasiswa', function(error, rows, fileds){
+    connection.query('select * from mahasiswa', function(error, rows, fields){
+        if(error){
+            connection.log(error);
+        }else {
+            response.ok(rows, res)
+        }
+    });
+};
+
+//menampilkan semua data mahasiswa berdasarkan id
+exports.tampilberdasarkanid = function(req,res){
+    let id = req.params.id;
+    connection.query('select * from mahasiswa where id_mahasiswa= ?', [id], function(error, rows, fields){
         if(error){
             connection.log(error);
         }else {
--- src/routes.js
+++ src/routes.js
@@ -8,4 +8,7 @@ module.exports = function(app){
     
     app.route('/tampil')
         .get(jsonku.tampilmahasiswa);
+    
+    app.route('/tampil/:id')
+        .get(jsonku.tampilberdasarkanid);
 }
