7c7731fb8540182c9677b842c37daab56ec67fc5
Taufik Rahman <taufikprincecode10@gmail.com>
2022-07-02 21:58:21 +0900

    membuat route menambah data

--- src/controller.js
+++ src/controller.js
@@ -21,11 +21,29 @@ exports.tampilmahasiswa = function(req,res){
 //menampilkan semua data mahasiswa berdasarkan id
 exports.tampilberdasarkanid = function(req,res){
     let id = req.params.id;
-    connection.query('select * from mahasiswa where id_mahasiswa= ?', [id], function(error, rows, fields){
+    connection.query('select * from mahasiswa where id_mahasiswa= ?', [id], 
+    function(error, rows, fields){
         if(error){
-            connection.log(error);
+            console.log(error);
         }else {
             response.ok(rows, res)
+        }
+    });
+};
+
+//menambahkan data mahasiswa
+exports.tambahmahasiswa = function(req, res){
+    let nim = req.body.nim;
+    let nama = req.body.nama;
+    let jurusan = req.body.jurusan;
+
+    connection.query('INSERT INTO mahasiswa (nim, nama, jurusan) VALUES(?, ?, ?)',
+    [nim, nama, jurusan],
+    function(error, rows, fields){
+        if(error){
+            console.log(error);
+        }else {
+            response.ok('Berhasil menambahkan data!',res);
         }
     });
 };
